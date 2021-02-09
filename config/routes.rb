7c139c0e30841805dcb1cc7a05ee38d6cc91b52f
kotaro.m <benkyou1212@gmail.com>
2021-02-09 02:05:12 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,17 @@ Rails.application.routes.draw do
  root to: 'toppages#index'
  
  get 'signup', to: 'users#new'
- resources :users, only: [:index, :show, :new, :create]
+ resources :users, only: [:index, :show, :new, :create] do
+   member do
+     get :followings
+     get :followers
+   end
+ end
  
  get 'login', to: 'sessions#new'
  post 'login', to: 'sessions#create'
  delete 'logout', to: 'sessions#destroy'
  
 resources :microposts, only: [:create, :destroy]
+resources :relationships, only: [:create, :destroy]
 end
